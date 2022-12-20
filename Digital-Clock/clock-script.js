ba919d1df9d851c88fb0fcb8f7f784388b2c08c2
KafiH <kafi.hassan1996@gmail.com>
2022-12-20 09:27:15 -0500

    Main Clock

--- Digital-Clock/clock-script.js
+++ Digital-Clock/clock-script.js
@@ -5,6 +5,17 @@ function displayTime(){
     var sec= dateTime.getSeconds();
     var session= document.getElementById('session');
 
+    if(hrs<10){
+        hrs="0"+hrs
+    }
+    if(min<10){
+        min="0"+min
+    }
+
+    if(sec<10){
+        sec="0"+sec
+    }
+
     if (hrs>=12){
         Selection.InnerHTML = 'PM';
     }else{
