22e2c8f81b44876973d581dcaef41a1a68c0f6c7
KafiH <kafi.hassan1996@gmail.com>
2022-12-21 12:49:41 -0500

    day 3

--- Timer/timer-script.js
+++ Timer/timer-script.js
@@ -1,5 +1,5 @@
-const selectMenu =document.querySelectorAll("select");
+const selectMenu = document.querySelectorAll("select");
 
-for (let i=12;i>0;i++){
+for (let i=12;i>0;i--){
 console.log(i)
 }
